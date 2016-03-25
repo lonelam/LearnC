f3c67e7bcf2914cfd1c20e120a0d4a7ebf096c08
Layman <laizenan@gmail.com>
2016-03-25 19:39:35 +0800

    OK,deleted the .exes for save memories

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # LearnC
 actually I'm gathering my mind or whatever.
+胡乱学了一些东西，也事实上荒废了好多时间，随意吧。
--- chap7/chap7/源.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-int main(void)
-{
-	using namespace std;
-	cout << "let it be";
-}
